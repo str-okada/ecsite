61ab13c17c7f984808abb498cf13229351fec973
str-okada <shotarookadaa@gmail.com>
2023-10-12 13:55:55 -0700

    user page been done

--- src/main/java/jp/co/internous/ecsite/controller/IndexController.java
+++ src/main/java/jp/co/internous/ecsite/controller/IndexController.java
@@ -15,9 +15,13 @@ import com.google.gson.Gson;
 
 import jp.co.internous.ecsite.model.domain.MstGoods;
 import jp.co.internous.ecsite.model.domain.MstUser;
+import jp.co.internous.ecsite.model.dto.HistryDto;
+import jp.co.internous.ecsite.model.form.CartForm;
+import jp.co.internous.ecsite.model.form.HistoryForm;
 import jp.co.internous.ecsite.model.form.LoginForm;
 import jp.co.internous.ecsite.model.mapper.MstGoodMapper;
 import jp.co.internous.ecsite.model.mapper.MstUserMapper;
+import jp.co.internous.ecsite.model.mapper.TblPurchaseMapper;
 
 @Controller
 @RequestMapping("/ecsite")
@@ -28,12 +32,15 @@ public class IndexController {
 	@Autowired
 	private MstUserMapper userMapper;
 	
+	@Autowired
+	private TblPurchaseMapper purchaseMapper;
+	
 	private Gson gson = new Gson();
 	
 	@GetMapping("/")
 	public String index(Model model) {
 		List<MstGoods> goods = goodsMapper.findAll();
-		model.addAttribute("goods",goods);
+		model.addAttribute("goods", goods);
 		
 		return "index";
 	}
@@ -42,7 +49,7 @@ public class IndexController {
 	@ResponseBody
 	@PostMapping("/api/login")
 	public String loginMapper(@RequestBody LoginForm f) {
-		MstUser user = userMapper.findByNameAndPassword(f);
+		MstUser user = userMapper.findByNameAndPassword( f );
 		
 		if(user == null) {
 			user = new MstUser();
@@ -51,5 +58,29 @@ public class IndexController {
 		
 		return gson.toJson(user);
 	}
+	
+	@ResponseBody
+	@PostMapping("/api/purchase")
+	public int purchaseApi(@RequestBody CartForm f) {
+		
+		f.getCartList().forEach((c) -> {
+			int total = c.getPrice() * c.getCount();
+			purchaseMapper.insert(f.getUserId(), c.getId(), c.getGoodsName(), c.getCount(), total);
+		});
+		
+		return f.getCartList().size();
+	}
+	
+	@ResponseBody
+	@PostMapping("/api/history")
+	public String historyApi(@RequestBody HistoryForm f) {
+		
+		int userId = f.getUserId();
+		List<HistryDto> history = purchaseMapper.findHistory(userId);
+		
+		return gson.toJson(history);
+	}
+	
+	
 
 }
--- src/main/java/jp/co/internous/ecsite/model/dto/HistryDto.java
+++ src/main/java/jp/co/internous/ecsite/model/dto/HistryDto.java
@@ -1,27 +1,35 @@
-package jp.co.internous.ecsite.model.domain;
+package jp.co.internous.ecsite.model.dto;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
-public class TblPurchase {
-	
+import jp.co.internous.ecsite.model.domain.TblPurchase;
+
+public class HistryDto {
 	private int id;
-	
 	private int userId;
-	
 	private int goodsId;
-	
 	private String goodsName;
-	
 	private int itemCount;
-	
 	private int total;
+	private String createAt;
 	
-	private Timestamp createAt;
+	public HistryDto() {}
+	
+	public HistryDto(TblPurchase entity) {
+		this.id = entity.getId();
+		this.userId = entity.getUserId();
+		this.goodsName = entity.getGoodsName();
+		this.itemCount = entity.getItemCount();
+		this.total = entity.getTotal();
+		Timestamp d = entity.getCreateAt();
+		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		this.createAt = f.format(d);	
+	}	
 	
 	public int getId() {
 		return id;
 	}
-	
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -29,7 +37,6 @@ public class TblPurchase {
 	public int getUserId() {
 		return userId;
 	}
-	
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
@@ -37,7 +44,6 @@ public class TblPurchase {
 	public int getGoodsId() {
 		return goodsId;
 	}
-	
 	public void setGoodsId(int goodsId) {
 		this.goodsId = goodsId;
 	}
@@ -45,7 +51,6 @@ public class TblPurchase {
 	public String getGoodsName() {
 		return goodsName;
 	}
-	
 	public void setGoodsName(String goodsName) {
 		this.goodsName = goodsName;
 	}
@@ -53,7 +58,6 @@ public class TblPurchase {
 	public int getItemCount() {
 		return itemCount;
 	}
-	
 	public void setItemCount(int itemCount) {
 		this.itemCount = itemCount;
 	}
@@ -61,16 +65,14 @@ public class TblPurchase {
 	public int getTotal() {
 		return total;
 	}
-	
 	public void setTotal(int total) {
 		this.total = total;
 	}
 	
-	public Timestamp getCreateAt() {
+	public String getCreateAt() {
 		return createAt;
 	}
-	
-	public void setCreateAt(Timestamp createAt) {
+	public void setCreateAt(String createAt) {
 		this.createAt = createAt;
 	}
 }
